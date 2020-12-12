e4ed1347eaa5772b876cac0686179187c347512c
Jiri Prochazka <jiriprochazka.96@gmail.com>
2020-12-12 17:40:05 +0100

    Low panel work

--- craftsman/mainwindow.py
+++ craftsman/mainwindow.py
@@ -6,15 +6,20 @@ LOG = logger.getLogger('qtpyvcp.' + __name__)
 
 from qtpy import QtGui
 import resources_rc
+import qtpyvcp.actions.machine_actions as actions
 
 class MyMainWindow(VCPMainWindow):
     """Main window class for the VCP."""
     def __init__(self, *args, **kwargs):
         super(MyMainWindow, self).__init__(*args, **kwargs)
-        self.UiComponents()
+        #self.UiComponents()
+	actions.jog.set_linear_speed(1)
 
 
     # add any custom methods here
     def UiComponents(self):
 		#self.playButton.setIcon(QtGui.QIcon('play.svg'))
 		pass
+
+
+
